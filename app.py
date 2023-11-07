ebfa5f28b58ee327c1f42780c216ffa33cd981b8
davidcapelle <davidcapelle33@free.fr>
2023-11-07 21:44:46 +0100

    comparison with real answer

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import ast
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-# solution_df = duckdb.sql(ANSWER_STR).df()
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review?",
@@ -20,26 +18,31 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE themes = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("Enter your code:")
 query = st.text_area(label="Your SQL code here", key="user_input")
 
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
-#
-#    try:
-#        result = result[solution_df.columns]
-#        st.dataframe(result.compare(solution_df))
-#    except KeyError as e:
-#        st.write("Some columns are missing")
-#
-#    n_lines_difference = result.shape[0] - solution_df.shape[0]
-#    if n_lines_difference != 0:
-#        st.write(
-#            f"Result has {n_lines_difference} lines difference with the solution_df"
-#       )
-#
-#
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"Result has {n_lines_difference} lines difference with the solution_df"
+            )
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
@@ -50,7 +53,5 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
+
     st.write(answer)
